24c8bc44c87047a506ba5d2b34ef403c5c058d96
Sunil Karki <karkisunil1200@gmail.com>
2020-04-07 16:21:31 -0500

    Finished with all the CRUD functionality

--- posts/router.js
+++ posts/router.js
@@ -69,4 +69,59 @@ router.post('/', (req, res) => {
     });
 });
 
+router.post('/:id/comments', (req, res) => {
+  const post = req.body;
+  const id = req.params.id;
+
+  db.insertComment(post)
+    .then(comment => {
+      if (comment) {
+        res.status(201).json(comment);
+      } else if (comment.post_id !== id) {
+        res.status(404).json({message: 'The post with the specified ID does not exist'});
+      } else {
+        res.status(400).json({message: 'Please provide text for the comment'});
+      }
+    })
+    .catch(err => {
+      res.status(500).json({
+        message: 'There was an error while saving the comment to the database',
+        error: err.message
+      });
+    });
+});
+
+router.put('/:id', (req, res) => {
+  const id = req.params.id;
+  const changes = req.body;
+
+  db.update(id, changes)
+    .then(post => {
+      if (post) {
+        res.status(200).json(post);
+      } else {
+        res.status(400).json({message: 'Please provide title and contents for the post'});
+      }
+    })
+    .catch(err => {
+      res.status(500).json({message: "The post couldn't not be modified", error: err.message});
+    });
+});
+
+router.delete('/:id', (req, res) => {
+  const {id} = req.params;
+
+  db.remove(id)
+    .then(post => {
+      if (post) {
+        res.status(200).json(post);
+      } else {
+        res.status(404).json({message: 'The post with the specified ID does not exist'});
+      }
+    })
+    .catch(err => {
+      res.status(500).json({message: 'The post could not be removed', error: err.message});
+    });
+});
+
 module.exports = router;
