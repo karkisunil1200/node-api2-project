f1ed4b21f2131890036464ff7c12f1e629af881a
Sunil Karki <karkisunil1200@gmail.com>
2020-04-07 15:13:41 -0500

    Server up and running

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const server = express();
+
+server.get('/', (req, res) => {
+  res.json({server: 'up'});
+});
+
+const PORT = 5000;
+server.listen(PORT, () => console.log(`running on port${PORT}`));
