bea85fc37c8e687b9e2cacbbb88e62a4563f6b30
Sunil Karki <karkisunil1200@gmail.com>
2020-04-07 16:25:45 -0500

    Added cors on the project

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const postRouter = require('./posts/router');
 
 const server = express();
 
 server.use(express.json());
+server.use(cors());
 
 server.get('/', (req, res) => {
   res.json({server: 'up'});
